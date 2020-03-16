e515a6b9d81a1462ef4ff986d3ebad5bf7ea7e35
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-16 12:18:26 +0100

    Add project files.

---
+++ variable operation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable_operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3;
+            Console.WriteLine("Enter your first number: ");
+            number1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            number2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your third number: ");
+            number3 = Int32.Parse(Console.ReadLine());
+            int results;
+            results = number1 + number2 + number3;
+            Console.WriteLine("Sum= "+results.ToString());
+            Console.ReadKey();
+            results = number1 - number2 - number3;
+            Console.WriteLine("Sum= " + results.ToString());
+            Console.ReadKey();
+            results = number1 * number2 * number3;
+            Console.WriteLine("Sum= " + results.ToString());
+            Console.ReadKey();
+            results = number1 / number2 / number3;
+            Console.WriteLine("Sum= " + results.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
